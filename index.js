4851c796ea0ba7a4a9c71b06c29dcce5b40ff786
hiba <hibzrashed@gmail.com>
2022-05-22 13:04:22 -0500

    commit tree 59f023230e20df7bc3fea0930e4c56d030b8da38

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+function appendCat(name) {
+    const addedCat = ([...cats, name])
+    return addedCat
+}
+function prependCat(name) {
+    const prependCat = [name, ...cats ]
+    return prependCat
+}
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
